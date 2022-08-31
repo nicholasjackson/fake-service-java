bd757547de6229ab184873ee88d007570436a45c
Nic Jackson <jackson.nic@gmail.com>
2022-08-31 09:56:17 +0100

    Ensure works with Wildfly server

--- src/main/java/io/nicholasjackson/fakeservice/FakeServiceApplication.java
+++ src/main/java/io/nicholasjackson/fakeservice/FakeServiceApplication.java
@@ -2,9 +2,16 @@ package io.nicholasjackson.fakeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FakeServiceApplication {
+public class FakeServiceApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+    return builder.sources(FakeServiceApplication.class);
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FakeServiceApplication.class, args);
